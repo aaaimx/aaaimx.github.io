ce461b7340eddb386df83b7cf5105e5864d068df
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-04-15 01:29:23 -0500

    Add deploy script to gitignore

--- bin/deploy.sh
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/bash
-git checkout master
-cp -rlf ./dist/static/ .
-rm -r ./dist/static/
-mv ./dist/index.html index.html
-git add . && git commit -m 'deploy to github-pages'
-git push -u origin master
