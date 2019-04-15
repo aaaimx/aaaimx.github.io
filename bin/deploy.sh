1ace42a720925c874f452d73281dbfd86da0df10
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-04-15 01:11:30 -0500

    deploy to github-pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/bash
 git checkout master
-cp -rlf ./dist/static/ ./static/
+cp -rlf ./dist/static/ .
 rm -r ./dist/static/
 mv ./dist/index.html index.html
 git add . && git commit -m 'deploy to github-pages'
