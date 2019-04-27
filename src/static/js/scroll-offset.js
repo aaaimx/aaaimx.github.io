6bbfe3879342004f98438dd17ab9845aa80e0a58
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-04-26 20:52:21 -0500

    Merge branch new template --> vuejs

--- src/static/js/scroll-offset.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-$(document).ready(function () {
-    $('a[href*=\\#]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 500);
-    });
-});
