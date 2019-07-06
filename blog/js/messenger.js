2314927e32ee8cb57529ddea8d3e8ab02153df04
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-07-06 17:51:53 -0500

    blog aaaimx

---
+++ blog/js/messenger.js
@@ -0,0 +1,17 @@
+/**
+ * Init Facebook Messanger Chat
+ */
+window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: 'v3.2'
+  })
+};
+
+(function (d, s, id) {
+  var js; var fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) return
+  js = d.createElement(s); js.id = id
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+  fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
---
+++ js/messages.js
@@ -0,0 +1,18 @@
+const messages = {
+  en: {
+    navbar: {
+      about_us: 'About Us'
+    },
+    recent_articles: 'Recent Articles',
+    older: 'Older',
+    newer: 'Newer'
+  },
+  es: {
+    navbar: {
+      about_us: 'Nosotros'
+    },
+    recent_articles: 'Publicaciones recientes',
+    older: 'Antiguos',
+    newer: 'Nuevos'
+  }
+}
