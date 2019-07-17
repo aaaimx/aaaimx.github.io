d9d8ff81a0bcd2521b7da1944f4a6c88af48bb4c
Raul Novelo <rnovelo.cruz98@gmail.com>
2019-07-16 21:10:56 -0500

    Lasted changes

--- js/app.js
+++ js/app.js
@@ -8,16 +8,7 @@ const i18n = new VueI18n({
 // Create a Vue instance with `i18n` option
 new Vue({
   i18n,
-  data: {
-    posts: []
-  },
   created() {
-    axios('https://jsonplaceholder.typicode.com/photos').then(response => {
-      this.posts = response.data
-      console.log(response)
-    }, err => {
-      console.log(err)
-    })
     var url_string = window.location.href
     var url = new URL(url_string);
     var c = url.searchParams.get("hello");
@@ -26,11 +17,6 @@ new Vue({
   methods: {
     changeLang(lang) {
       this.$i18n.locale = lang
-    }
-  },
-  computed: {
-    last() {
-      return this.posts.slice(0, 5)
     }
   }
 }).$mount('#app')
