626efcb8b961e7e14650b0426fc6b1f267a5f342
PAUL SPARTAN <carmesi.services@gmail.com>
2019-07-22 04:35:49 -0500

    se agregó el lenguaje español-ingles

--- js/messages.js
+++ js/messages.js
@@ -1,18 +1,153 @@
 const messages = {
   en: {
     navbar: {
-      about_us: 'About Us'
+      about_us1: 'About Us',
+      who: 'who we are?',
+      partners1: 'Partners & Sponsors',
+      memb: 'Members',
+      execu_comit1: 'Executive Committee',
+      board1: 'Board',
+      div: 'Divisions',
+      machine: 'Machine Learning',
+      soft: 'Software',
+      rob_elec: 'Robotic & Electronic',
+      operat: 'Operations',
+      found: 'Founders',
+      res: 'Research',
+      contact: 'Contact',
+      lang1: 'English',
+      lang2: 'Spanish',
+
     },
-    recent_articles: 'Recent Articles',
-    older: 'Older',
-    newer: 'Newer'
+    intro_section: {
+      logo_p1: 'AAAI Student Chapter',
+      logo_p2: 'at Yucatán, México',
+      itm: 'Mérida Institute of Technology',
+    },
+    services_section: {
+      about_us2: 'About us',
+      message_ab: 'We are an association that care about the technological development of the country, contributing to more students have knowledge about artificial intelligence and other related fields that are becoming more popular.',
+      mission: 'Mission',
+      mission_desc: 'Our goal is to promote the knowledge and practice of AI in both the ITM and the local community, encouraging more students to join and promote research and technological developments.',
+      vision: 'Vision',
+      vision_desc: 'We propose to achieve this through a solid core of researchers from both ITM and leading research centers such as CIMAT and CentroGeo, who will bring IA knowledge to students, in the form of conferences, symposiums, meetings, etc., but mainly, through workshops and courses, whose contents will be replicated to future generations of students.',
+      community: 'Community',
+      community_desc: 'We are a community dedicated to increase knowledge and promote greater awareness of Artificial Intelligence, located (mainly) in Mérida, Yucatán, México. The headquarters of the chapter are located at the Mérida Institute of Technology (Instituto Tecnológico de Mérida).',
+    },
+    clients_section: {
+      partners2: 'Partners & Sponsors',
+      partners_desc: 'We work with several companies and institutes in Yucatán, México, focused on Artificial Intelligence, Applied Maths and Software development projects.',
+    },
+    testimonials_section: {
+      execu_comit2: 'EXECUTIVE COMMITTEE',
+      execu_comit2_desc: 'We are a group of students and researchers from the ITM along with researchers from the Center for Mathematical Research (CIMAT) and CentroGeo, some of the leading educational and research centers in Yucatán, México.',
+      ml_leader: 'Machine Learning Leader',
+      sof_leader: 'Software Leader',
+      rob_leader: 'Robotic/Electronic Leader',
+      math_leader: 'Applied Mathematics Leader',
+      ope_leader: 'Operations Leader',
+      web_leader: 'Web Master',
+      basic_member1: 'SC Member',
+    },
+    our_team_section: {
+      board2: 'BOARD',
+      co_chair: 'Co-Chair / CentroGeo Researcher',
+      chair: 'Chair / ITM Researcher',
+      secretary: 'Secretary / CIMAT Researcher',
+    },
+    call_to_action_section: {
+      des_main: 'The chapter is focused on scientific research and dissemination, hence we understand that there are different ways of thinking and as well as different points of view, all this in a margin of respect regardless of race, ideology, socioeconomic status, sex, physical condition or gender identity.',
+      des_sec: '"Sometimes people from whom nobody expects anything, is the one who does things that nobody can imagine"',
+      say: '- Anonymous',
+      discover: 'Discover...',
+    },
+    contact_section: {
+      contact_us: 'Contact Us',
+      contact_us_desc: 'Do you have a particular challenge you are currently dealing with? Contact us today and explore what we can do for you.',
+      address: 'Address',
+      address_inf: 'Mérida Institute of Technology, West Campus, Yucatán, México.',
+      AAAI_chap: 'AAAI Student Chapter México',
+      email: 'Email',
+    },
+    footer: {
+      arr: '. All Rights Reserved',
+      pwby: 'Powered by',
+    }
+
   },
   es: {
     navbar: {
-      about_us: 'Nosotros'
+      about_us1: 'Nosotros',
+      who: 'quienes somos',
+      partners1: 'Socios y Patrocinadores',
+      memb: 'Miembros',
+      execu_comit1: 'Comité Ejecutivo',
+      board1: 'Panel',
+      div: 'Divisiones',
+      machine: 'Aprendizaje Automatizado',
+      soft: 'Software',
+      rob_elec: 'Robotica y Electronica',
+      operat: 'Operaciones',
+      found: 'Fundadores',
+      res: 'Investigaciones',
+      contact: 'Contacto',
+      lang1: 'Inglés',
+      lang2: 'Español',
+
     },
-    recent_articles: 'Publicaciones recientes',
-    older: 'Antiguos',
-    newer: 'Nuevos'
+    intro_section: {
+      logo_p1: 'AAAI Capítulo Estudiantil',
+      logo_p2: 'Sede Yucatán, Mexico',
+      itm: 'Instituto Tecnológico de Mérida',
+    },
+    services_section: {
+      about_us2: 'Nosotros',
+      message_ab: 'Somos una asociación que se preocupa por el desarrollo tecnológico del país, contribuyendo a que más estudiantes tengan conocimiento sobre inteligencia artificial y otros campos relacionados que se están volviendo más populares.',
+      mission: 'Mision',
+      mission_desc: 'Nuestro objetivo es promover el conocimiento y la práctica de la IA tanto en el ITM como en la comunidad local, alentando a más estudiantes a unirse y promover la investigación y los avances tecnológicos.',
+      vision: 'Vision',
+      vision_desc: 'Proponemos lograr esto a través de un sólido núcleo de investigadores tanto de ITM como de centros de investigación líderes como CIMAT y CentroGeo, quienes aportarán el conocimiento de AI a los estudiantes, en forma de conferencias, simposios, reuniones, etc., pero principalmente a través de talleres. y cursos, cuyos contenidos serán replicados a futuras generaciones de estudiantes.',
+      community: 'Comunidad',
+      community_desc: 'Somos una comunidad dedicada a aumentar el conocimiento y promover una mayor conciencia de la Inteligencia Artificial, ubicada (principalmente) en Mérida, Yucatán, México. La sede del capítulo se encuentra en el Instituto Tecnológico de Mérida.',
+    },
+    clients_section: {
+      partners2: 'Socios y Patrocinadores',
+      partners_desc: 'Trabajamos con varias compañías e institutos en Yucatán, México, enfocados en proyectos de inteligencia artificial, matemática aplicada y desarrollo de software.',
+    },
+    testimonials_section: {
+      execu_comit2: 'COMITÉ EJECUTIVO',
+      execu_comit2_desc: 'Somos un grupo de estudiantes e investigadores del ITM junto con investigadores del Centro de Investigación Matemática (CIMAT) y CentroGeo, algunos de los principales centros educativos y de investigación en Yucatán, México.',
+      ml_leader: 'Lider De Aprendizaje Automatizado',
+      sof_leader: 'Lider De Software',
+      rob_leader: 'Lider De Robotica/Electronica',
+      math_leader: 'Líder De Matemáticas Aplicadas',
+      ope_leader: 'Lider De Operaciones',
+      web_leader: 'Lider De Desarrollo Web',
+      basic_member1: 'Miembro',
+    },
+    our_team_section: {
+      board2: 'Panel',
+      co_chair: 'Vicepresidente / Investigador del CentroGeo.',
+      chair: 'Presidente / Investigador del ITM',
+      secretary: 'Secretario / Investigador del CIMAT.',
+    },
+    call_to_action_section: {
+      des_main: 'El capítulo se enfoca a la investigación y divulgación científica, por lo que entendemos que existen diferentes formas de pensar al igual que diversos puntos de vista, todo esto en un margen de respeto sin importar raza, ideología, condición fisica o identidad de género.',
+      des_sec: '"A veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar"',
+      say: '- Anonimo',
+      discover: 'Saber más...',
+    },
+    contact_section: {
+      contact_us: 'Contáctenos',
+      contact_us_desc: '¿Tienes un desafío particular con el que te estás enfrentando actualmente? Contáctanos hoy y explora lo que podemos hacer por ti.',
+      address: 'Instituto Tecnólogico de Mérida Campus Poniente, Yucatán, México.',
+      address_inf: 'Instituto Tecnólogico de Mérida Campus Poniente, Yucatán, México',
+      AAAI_chap: 'AAAI Student Chapter México',
+      email: 'Correo',
+    },
+    footer: {
+      arr: '. Todos Los Derechos Reservados.',
+      pwby: 'Desarrollado Por',
+    }
   }
 }
