4316962df7d57d031d66a8a0b3497482f62b7278
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-12-13 12:57:04 -0600

    Publishing folder _site to master in 12/13/2019, 12:57:03 PM

---
+++ get-data.js
@@ -0,0 +1,64 @@
+// file system module to perform file operations
+const fs = require('fs')
+const axios = require('axios')
+
+// json data
+var jsonData = []
+
+function getImgUrl (url) {
+  var result = ''
+  try {
+    var res = url.split('https://drive.google.com/file/d/')
+    res = res[1]
+    res = res.split('/view?usp=drivesdk')
+    result = 'https://drive.google.com/uc?id=' + res[0]
+  } catch (error) {
+    console.log(error)
+  }
+  return result
+}
+
+axios
+  .get('https://aaaimx-admin.herokuapp.com/api/members/?panel=true&limit=20')
+  .then(res => {
+    var members = res.data.results
+    members.forEach(m => {
+      const { surname, name, board, committee, thumbnailFile, charge } = m
+      var attributes = {}
+      var photo = '/img/people/default-avatar.jpg'
+      if (thumbnailFile) {
+        photo = getImgUrl(thumbnailFile)
+      }
+      if (committee) {
+        attributes = {
+          surname: surname.split('-')[0],
+          name: name.split(' ')[0],
+          thumbnailUrl: photo,
+          committee,
+          charge
+        }
+        jsonData.push(attributes)
+      } else if (board) {
+        attributes = {
+          surname,
+          name,
+          thumbnailUrl: photo,
+          board,
+          charge
+        }
+        jsonData.push(attributes)
+      }
+    })
+    // stringify JSON Object
+    var jsonContent = JSON.stringify(jsonData, null, 4)
+    console.log(jsonContent)
+
+    fs.writeFile('./_data/team.json', jsonContent, 'utf8', function (err) {
+      if (err) {
+        console.log('An error occured while writing JSON Object to File.')
+        return console.log(err)
+      }
+
+      console.log('JSON file has been saved.')
+    })
+  })
